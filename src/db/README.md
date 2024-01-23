a9b9c65dbbfc23d500fec21e2b6becaace199ea0
Denux <timon.klinkert@denux.dev>
2024-01-23 08:23:59 +0100

    improved data structure
    - allow multiple users per account and multiple account per user (m to m relation)

--- src/db/README.md
+++ src/db/README.md
@@ -5,14 +5,6 @@
 ```mermaid
 
 erDiagram
-	User {
-		int id PK
-		string username UK
-		string email UK "Nullable"
-		string password UK "Hashed and salted Password"
-		timestamp created_at "NotNull"
-	}
-
 	Currency {
 		int id UK
 		string name "NotNull"
@@ -20,19 +12,36 @@ erDiagram
 		string iso_code PK "NotNull"
 		int decimal_places "NotNull"
 	}
-	
+
+	User {
+		int id PK
+		string username UK
+		string email UK "Nullable"
+		string password UK "Hashed and salted Password"
+		timestamp created_at "NotNull"
+	}
+	User }o--o{ Account: "many to many"
+
 	Account {
 		int id PK
 		User owner FK "NotNull"
 		string name UK "NotNull"
 		string description "Nullable"
+		string iban UK "Nullable"
 		int balance "NotNull"
 		Currency currency FK "NotNull"
 		timestamp created_at "NotNull"
 	}
-	Account ||--|| User : "one to one"
-	Account ||--|| Currency : "one to one"
-	
+	Account }o--o{ User: "many to many"
+	Account ||--|| Currency: "one to one"
+
+	UserAccount {
+		User user PK "References User.id"
+		Account account PK "References Account.id"
+	}
+	UserAccount ||--|| User: "one to one"
+	UserAccount ||--|| Account: "one to one"
+
 	Transaction {
 		int id PK
 		Account source FK "Nullable"
@@ -43,7 +52,7 @@ erDiagram
 		timestamp created_at "NotNull"
 		timestamp executed_at "NotNull"
 	}
-	Transaction ||--|| Account : "one to one"
+	Transaction ||--|| Account: "one to one"
 ```
 ## SQL
 
--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,40 +1,48 @@
-CREATE TABLE "user"
-(
-    id         SERIAL PRIMARY KEY,
-    username   TEXT UNIQUE NOT NULL,
-    email      TEXT UNIQUE,
-    password   TEXT        NOT NULL,
-    created_at TIMESTAMP   NOT NULL DEFAULT current_timestamp
-);
-
 CREATE TABLE currency
 (
-    id             SERIAL PRIMARY KEY,
-    name           TEXT        NOT NULL,
-    symbol         TEXT        NOT NULL,
-    iso_code       TEXT UNIQUE NOT NULL,
-    decimal_places INTEGER     NOT NULL
+	id             SERIAL PRIMARY KEY,
+	name           TEXT        NOT NULL,
+	symbol         TEXT        NOT NULL,
+	iso_code       TEXT UNIQUE NOT NULL,
+	decimal_places INTEGER     NOT NULL
+);
+
+CREATE TABLE "user"
+(
+	id         SERIAL PRIMARY KEY,
+	username   TEXT UNIQUE NOT NULL,
+	email      TEXT UNIQUE,
+	password   TEXT        NOT NULL,
+	created_at TIMESTAMP   NOT NULL DEFAULT current_timestamp
 );
 
 CREATE TABLE account
 (
-    id          SERIAL PRIMARY KEY,
-    user_id     INTEGER REFERENCES "user" (id)   NOT NULL,
-    name        TEXT UNIQUE                      NOT NULL,
-    description TEXT,
-    balance     INTEGER                          NOT NULL DEFAULT 0,
-    currency    INTEGER REFERENCES Currency (id) NOT NULL,
-    created_at  TIMESTAMP                        NOT NULL DEFAULT current_timestamp
+	id          SERIAL PRIMARY KEY,
+	owner       INTEGER REFERENCES "user" (id)   NOT NULL,
+	name        TEXT UNIQUE                      NOT NULL,
+	description TEXT,
+	iban        TEXT UNIQUE,
+	balance     INTEGER                          NOT NULL DEFAULT 0,
+	currency    INTEGER REFERENCES Currency (id) NOT NULL,
+	created_at  TIMESTAMP                        NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE user_account
+(
+	user_id    INTEGER REFERENCES "user" (id) ON UPDATE CASCADE ON DELETE CASCADE,
+	account_id INTEGER REFERENCES Account (id) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY (user_id, account_id)
 );
 
 CREATE TABLE transaction
 (
-    id          SERIAL PRIMARY KEY,
-    source      INTEGER REFERENCES Account (id),
-    destination INTEGER REFERENCES Account (id),
-    amount      INTEGER                          NOT NULL,
-    currency    INTEGER REFERENCES Currency (id) NOT NULL,
-    description TEXT,
-    created_at  TIMESTAMP                        NOT NULL DEFAULT current_timestamp,
-    executed_at TIMESTAMP                        NOT NULL DEFAULT current_timestamp
+	id          SERIAL PRIMARY KEY,
+	source      INTEGER REFERENCES Account (id),
+	destination INTEGER REFERENCES Account (id),
+	amount      INTEGER                          NOT NULL,
+	currency    INTEGER REFERENCES Currency (id) NOT NULL,
+	description TEXT,
+	created_at  TIMESTAMP                        NOT NULL DEFAULT current_timestamp,
+	executed_at TIMESTAMP                        NOT NULL DEFAULT current_timestamp
 );
