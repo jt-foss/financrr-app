06285a07c2da113d85fcc63eea7ef35515a14dfd
Jason Dean Lessenich <jasonlessenich@gmail.com>
2024-01-28 17:17:57 +0100

    Initial commit

---
+++ example/restrr_example.dart
@@ -0,0 +1,6 @@
+import 'package:restrr/restrr.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/restrr_test.dart
@@ -0,0 +1,16 @@
+import 'package:restrr/restrr.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
