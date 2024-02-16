004cf91245cf88b97e52779c3ec24c687397fa90
Jason Dean Lessenich <jasonlessenich@gmail.com>
2024-02-16 23:20:43 +0100

    Started work on implemented Host URL check

--- lib/src/restrr_base.dart
+++ lib/src/restrr_base.dart
@@ -8,6 +8,7 @@ class RestrrOptions {
 
 class RestrrBuilder {
   final SessionInitType sessionInitType;
+  final String hostUrl;
   String? sessionId;
   String? email;
   String? password;
@@ -15,11 +16,11 @@ class RestrrBuilder {
 
   RestrrOptions options = RestrrOptions();
 
-  RestrrBuilder.refresh({required this.sessionId}) : sessionInitType = SessionInitType.refresh;
+  RestrrBuilder.refresh({required this.hostUrl, required this.sessionId}) : sessionInitType = SessionInitType.refresh;
 
-  RestrrBuilder.login({required this.email, required this.password, this.mfaCode}) : sessionInitType = SessionInitType.login;
+  RestrrBuilder.login({required this.hostUrl, required this.email, required this.password, this.mfaCode}) : sessionInitType = SessionInitType.login;
 
-  RestrrBuilder.register({required this.email, required this.password, this.mfaCode})
+  RestrrBuilder.register({required this.hostUrl, required this.email, required this.password, this.mfaCode})
       : sessionInitType = SessionInitType.register;
 
   Future<RestResponse<RestrrImpl>> create() async {
@@ -31,9 +32,23 @@ class RestrrBuilder {
 abstract class Restrr {
   /// Getter for the [EntityBuilder] of this [Restrr] instance.
   EntityBuilder get entityBuilder;
+
+  static Future<HostUrlCheckResult> checkHostUri(Uri hostUri) async {
+    return HostUrlCheckResult.healthy;
+  }
+
+  static Future<HostUrlCheckResult> checkHostUrl(String hostUrl) async {
+    final Uri? uri = Uri.tryParse(hostUrl);
+    if (uri == null) {
+      return HostUrlCheckResult.invalidUri;
+    }
+    return checkHostUri(uri);
+  }
 }
 
 class RestrrImpl implements Restrr {
   @override
   late final EntityBuilder entityBuilder = EntityBuilder(api: this);
 }
+
+enum HostUrlCheckResult { healthy, unhealthy, invalidUri, unknown }
