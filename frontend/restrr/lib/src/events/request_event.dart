7054ab3b24486077b01b1e218a31f368f6e6c093
Jason Dean Lessenich <jasonlessenich@gmail.com>
2024-03-13 00:30:33 +0100

    chore(repo): Removed `restrr` package

--- frontend/restrr/lib/src/events/request_event.dart
+++
@@ -1,8 +0,0 @@
-import 'package:restrr/restrr.dart';
-
-class RequestEvent extends RestrrEvent {
-  final String route;
-  final int? statusCode;
-
-  const RequestEvent({required super.api, required this.route, this.statusCode});
-}
--- frontend/restrr/lib/src/utils/string_utils.dart
+++
@@ -1,8 +0,0 @@
-class StringUtils {
-  const StringUtils._();
-
-  /// Returns the number of occurrences of [search] in [str].
-  static int count(String str, String search) {
-    return search.allMatches(str).length;
-  }
-}
