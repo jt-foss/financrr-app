dd778bacd67ac27b3e706e774bd6f5bd740abf08
jasonlessenich <jasonlessenich@gmail.com>
2024-03-14 21:16:43 +0000

    Deploying to gh-pages from @ financrr/financrr-app@da998492e2d210069ca5084eda9ac9f9910534c7 🚀

--- frontend-stage/flutter_service_worker.js
+++ frontend-stage/flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "04366f06731dc0d0f66811d220ac5501",
-"/": "04366f06731dc0d0f66811d220ac5501",
+const RESOURCES = {"index.html": "ec96cd1bc924bf40d8ff17667d4f9a52",
+"/": "ec96cd1bc924bf40d8ff17667d4f9a52",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
