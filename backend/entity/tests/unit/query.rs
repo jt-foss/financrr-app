7ee5699c40e29a4403e0c40064e80d5652264985
Denux <timon.klinkert@denux.dev>
2024-04-01 16:36:03 +0200

    fixed getting all transactions

--- backend/entity/tests/unit/query.rs
+++ backend/entity/tests/unit/query.rs
@@ -1,6 +1,6 @@
 use sea_orm::{DatabaseBackend, QueryTrait};
 
-use entity::account;
+use entity::{account, transaction};
 
 #[test]
 fn test_find_all_accounts_by_user_id() {
@@ -23,4 +23,15 @@ fn test_find_by_id_and_user_id() {
     let expected_sql = "SELECT \"account\".\"id\", \"account\".\"name\", \"account\".\"description\", \"account\".\"iban\", \"account\".\"balance\", \"account\".\"original_balance\", \"account\".\"currency\", \"account\".\"created_at\" FROM \"account\" INNER JOIN \"permissions\" ON \"permissions\".\"entity_id\" = \"account\".\"id\" AND \"entity_type\" = 'account' WHERE \"permissions\".\"user_id\" = 1 AND \"account\".\"id\" = 13";
 
     assert_eq!(actual_sql, expected_sql);
+}
+
+#[test]
+fn test_find_all_transactions_by_user_id() {
+    let user_id = 1;
+    let query = transaction::Entity::find_all_by_user_id(user_id);
+    let actual_sql = query.build(DatabaseBackend::Postgres).to_string();
+
+    let expected = "SELECT \"transaction\".\"id\", \"transaction\".\"source\", \"transaction\".\"destination\", \"transaction\".\"amount\", \"transaction\".\"currency\", \"transaction\".\"name\", \"transaction\".\"description\", \"transaction\".\"budget\", \"transaction\".\"created_at\", \"transaction\".\"executed_at\" FROM \"transaction\" INNER JOIN \"permissions\" ON \"permissions\".\"entity_id\" = \"transaction\".\"id\" WHERE \"permissions\".\"user_id\" = 1 AND \"permissions\".\"entity_type\" = 'transaction'";
+
+    assert_eq!(actual_sql, expected);
 }
