fb78cdb3829e493572aeb02338ed2d89fbfdb356
Jason Dean Lessenich <jasonlessenich@gmail.com>
2024-04-04 16:10:50 +0200

    Updated logo

--- frontend/buildAndUpload.sh
+++ frontend/buildAndUpload.sh
@@ -4,6 +4,6 @@ flutter pub get &&
 # Build & Upload iOS
 
 flutter build ipa --obfuscate --split-debug-info=debug_info &&
-xcrun altool --upload-app --type ios -f build/ios/ipa/*.ipa --apiKey M8Z6SQRJSH --apiIssuer bbb652dd-4490-4912-b117-423cae11d325 &&
+xcrun altool --upload-app --type ios -f build/ios/ipa/*.ipa --apiKey FCNM7TV5AK --apiIssuer 9717842e-73fe-4f11-bb0b-22eced17b1db &&
 cd ios &&
 cd .. || exit
