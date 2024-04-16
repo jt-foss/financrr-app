e4f2a7171a14489ef4271f1da03daded180f3dc2
Jason Dean Lessenich <jasonlessenich@gmail.com>
2024-04-16 11:25:30 +0200

    feature(frontend): Further enhanced FallbackErrorApp & build script

---
+++ frontend/lib/widgets/fallback_error_app.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class FallbackErrorApp extends StatelessWidget {
+  static const String githubIssueUrl =
+      'https://github.com/financrr/financrr-app/issues/new?labels=fix&template=bug_report.md&title=bug(frontend): ';
+
+  final String error;
+  final String? stackTrace;
+
+  const FallbackErrorApp({super.key, required this.error, this.stackTrace});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: GestureDetector(
+            onTap: () => Clipboard.setData(ClipboardData(text: error)),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: ListView(
+                children: [
+                  const Icon(Icons.error_outline_rounded),
+                  const SizedBox(height: 10),
+                  const Text('An error occurred while initializing the app',
+                      textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.w700)),
+                  Text.rich(
+                      textAlign: TextAlign.center,
+                      TextSpan(
+                        text: 'If this error persists, please create an',
+                        children: [
+                          WidgetSpan(
+                            child: GestureDetector(
+                                onTap: () => launchUrl(Uri.parse(githubIssueUrl)),
+                                child: const Text(' issue on our GitHub repository', style: TextStyle(color: Colors.blue))),
+                          ),
+                          const TextSpan(
+                            text: ' or contact us via support@financrr.app'
+                          )
+                        ],
+                      )),
+                  const SizedBox(height: 20),
+                  FutureBuilder(
+                    future: PackageInfo.fromPlatform(),
+                    builder: (context, snap) {
+                      if (!snap.hasData) return const SizedBox();
+                      final PackageInfo info = snap.data!;
+                      return Align(
+                        alignment: Alignment.centerRight,
+                        child: Text('v${info.version}+${info.buildNumber}', textAlign: TextAlign.center),
+                      );
+                    },
+                  ),
+                  Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.red.withOpacity(.1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text('${error.trim()}${stackTrace != null ? '\n\n${stackTrace!.trim()}' : ''}'))
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
