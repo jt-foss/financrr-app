52a730d9244dbd79bd04cc717f46e3332514cc94
Jason Dean Lessenich <jasonlessenich@gmail.com>
2024-04-16 11:26:09 +0200

    removed unnecessary import

--- frontend/lib/widgets/fallback_error_app.dart
+++ frontend/lib/widgets/fallback_error_app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 class FallbackErrorApp extends StatelessWidget {
   static const String githubIssueUrl =
