ddd207d9e12fdc0238d3579886fed26180419784
Jason Dean Lessenich <jasonlessenich@gmail.com>
2024-05-08 10:56:04 +0200

    Cleaned up ThemeSettingsPage

--- frontend/lib/modules/settings/views/theme_settings_page.dart
+++ frontend/lib/modules/settings/views/theme_settings_page.dart
@@ -6,7 +6,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../shared/ui/adaptive_scaffold.dart';
 import '../../../routing/page_path.dart';
 import '../models/theme.model.dart';
-import '../models/theme.state.dart';
 import '../models/theme_loader.dart';
 import 'settings_page.dart';
 
@@ -24,70 +23,70 @@ class _ThemeSettingsPageState extends ConsumerState<ThemeSettingsPage> {
   Widget build(BuildContext context) {
     var theme = ref.watch(themeProvider);
 
-    return AdaptiveScaffold(
-      resizeToAvoidBottomInset: false,
-      verticalBuilder: (_, __, size) => SafeArea(child: _buildVerticalLayout(size, theme)),
-    );
-  }
+    buildThemePreview(AppTheme appTheme) {
+      final bool currentTheme = ref.currentTheme.id == appTheme.id;
+      final bool activeLight = theme.lightTheme.id == appTheme.id;
+      final bool activeDark = theme.darkTheme.id == appTheme.id;
+      return Card.outlined(
+        child: ListTile(
+          onTap: () {
+            if (appTheme.themeMode == ThemeMode.light) {
+              ref.read(themeProvider.notifier).setLightTheme(appTheme);
+            } else {
+              ref.read(themeProvider.notifier).setDarkTheme(appTheme);
+            }
+            ref.read(themeProvider.notifier).setMode(appTheme.themeMode);
+          },
+          contentPadding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          title: Text(appTheme.effectiveName),
+          leading: CircleAvatar(
+            backgroundColor: appTheme.previewColor,
+            child: Icon(
+                activeLight
+                    ? Icons.wb_sunny
+                    : activeDark
+                    ? Icons.nightlight_round
+                    : null,
+                size: 17,
+                color: appTheme.themeMode == ThemeMode.light ? Colors.black : Colors.white),
+          ),
+          subtitle: activeLight || activeDark
+              ? (activeLight ? L10nKey.appearanceCurrentLightTheme : L10nKey.appearanceCurrentDarkTheme).toText()
+              : null,
+          trailing: currentTheme ? const Icon(Icons.check) : null,
+        ),
+      );
+    }
 
-  Widget _buildVerticalLayout(Size size, ThemeState themeState) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10),
-      child: Center(
-        child: SizedBox(
-          width: size.width / 1.1,
-          child: ListView(
-            children: [
-              ListTile(
-                  title: L10nKey.appearanceUseDeviceTheme.toText(),
-                  trailing: Switch(
-                    value: themeState.mode == ThemeMode.system,
-                    onChanged: (value) =>
-                        ref.read(themeProvider.notifier).setMode(value ? ThemeMode.system : ref.currentTheme.themeMode),
-                  ),
-                  subtitle: L10nKey.appearanceCurrentDeviceTheme
-                      .toText(namedArgs: {'deviceTheme': WidgetsBinding.instance.platformDispatcher.platformBrightness.name})),
-              const Divider(),
-              for (AppTheme theme in AppThemeLoader.themes) _buildThemePreview(theme, themeState)
-            ],
+    buildVerticalLayout(Size size) {
+      return Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Center(
+          child: SizedBox(
+            width: size.width / 1.1,
+            child: ListView(
+              children: [
+                ListTile(
+                    title: L10nKey.appearanceUseDeviceTheme.toText(),
+                    trailing: Switch(
+                      value: theme.mode == ThemeMode.system,
+                      onChanged: (value) =>
+                          ref.read(themeProvider.notifier).setMode(value ? ThemeMode.system : ref.currentTheme.themeMode),
+                    ),
+                    subtitle: L10nKey.appearanceCurrentDeviceTheme
+                        .toText(namedArgs: {'deviceTheme': WidgetsBinding.instance.platformDispatcher.platformBrightness.name})),
+                const Divider(),
+                for (AppTheme theme in AppThemeLoader.themes) buildThemePreview(theme)
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
+    }
 
-  Widget _buildThemePreview(AppTheme theme, ThemeState themeState) {
-    final bool currentTheme = ref.currentTheme.id == theme.id;
-    final bool activeLight = themeState.lightTheme.id == theme.id;
-    final bool activeDark = themeState.darkTheme.id == theme.id;
-    return Card.outlined(
-      child: ListTile(
-        onTap: () {
-          if (theme.themeMode == ThemeMode.light) {
-            ref.read(themeProvider.notifier).setLightTheme(theme);
-          } else {
-            ref.read(themeProvider.notifier).setDarkTheme(theme);
-          }
-          ref.read(themeProvider.notifier).setMode(theme.themeMode);
-        },
-        contentPadding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-        title: Text(theme.effectiveName),
-        leading: CircleAvatar(
-          backgroundColor: theme.previewColor,
-          child: Icon(
-              activeLight
-                  ? Icons.wb_sunny
-                  : activeDark
-                      ? Icons.nightlight_round
-                      : null,
-              size: 17,
-              color: theme.themeMode == ThemeMode.light ? Colors.black : Colors.white),
-        ),
-        subtitle: activeLight || activeDark
-            ? (activeLight ? L10nKey.appearanceCurrentLightTheme : L10nKey.appearanceCurrentDarkTheme).toText()
-            : null,
-        trailing: currentTheme ? const Icon(Icons.check) : null,
-      ),
+    return AdaptiveScaffold(
+      resizeToAvoidBottomInset: false,
+      verticalBuilder: (_, __, size) => SafeArea(child: buildVerticalLayout(size)),
     );
   }
 }
