65687fe5fced6daaeee4e843a7bdf52929fe492e
Jason Dean Lessenich <jasonlessenich@gmail.com>
2024-05-13 23:22:00 +0200

    Started work on implementing non-material themes

--- frontend/lib/modules/settings/views/l10n_settings_page.dart
+++ frontend/lib/modules/settings/views/l10n_settings_page.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:easy_localization/easy_localization.dart';
-import 'package:financrr_frontend/routing/navbar_shell.dart';
 import 'package:financrr_frontend/utils/extensions.dart';
 import 'package:financrr_frontend/utils/l10n_utils.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +9,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../shared/ui/adaptive_scaffold.dart';
 import '../../../routing/page_path.dart';
+import '../../../routing/navbar_shell.dart';
 import '../providers/l10n.provider.dart';
 import 'settings_page.dart';
 
--- frontend/lib/routing/navbar_shell.dart
+++ frontend/lib/routing/navbar_shell.dart
@@ -1,5 +1,6 @@
 import 'package:financrr_frontend/modules/settings/providers/theme.provider.dart';
 import 'package:financrr_frontend/utils/extensions.dart';
+import 'package:financrr_frontend/utils/l10n_utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -18,52 +19,6 @@ class ScaffoldNavBarShell extends StatefulWidget {
 }
 
 class ScaffoldNavBarShellState extends State<ScaffoldNavBarShell> {
-  static const List<NavigationDestination> _navBarDestinations = [
-    NavigationDestination(
-      icon: Icon(Icons.dashboard_outlined),
-      selectedIcon: Icon(Icons.dashboard_rounded),
-      label: 'Dashboard',
-    ),
-    NavigationDestination(
-      icon: Icon(Icons.account_balance_wallet_outlined),
-      selectedIcon: Icon(Icons.account_balance_wallet_rounded),
-      label: 'Accounts',
-    ),
-    NavigationDestination(
-      icon: Icon(Icons.leaderboard_outlined),
-      selectedIcon: Icon(Icons.leaderboard_rounded),
-      label: 'Statistics',
-    ),
-    NavigationDestination(
-      icon: Icon(Icons.person_outline_rounded),
-      selectedIcon: Icon(Icons.person_rounded),
-      label: 'Settings',
-    ),
-  ];
-
-  static const List<NavigationRailDestination> _navRailDestinations = [
-    NavigationRailDestination(
-      icon: Icon(Icons.dashboard_outlined),
-      selectedIcon: Icon(Icons.dashboard_rounded),
-      label: Text('Dashboard'),
-    ),
-    NavigationRailDestination(
-      icon: Icon(Icons.account_balance_wallet_outlined),
-      selectedIcon: Icon(Icons.account_balance_wallet_rounded),
-      label: Text('Accounts'),
-    ),
-    NavigationRailDestination(
-      icon: Icon(Icons.leaderboard_outlined),
-      selectedIcon: Icon(Icons.leaderboard_rounded),
-      label: Text('Statistics'),
-    ),
-    NavigationRailDestination(
-      icon: Icon(Icons.settings_outlined),
-      selectedIcon: Icon(Icons.settings_rounded),
-      label: Text('Settings'),
-    ),
-  ];
-
   bool _isHovered = false;
 
   @override
@@ -82,7 +37,7 @@ class ScaffoldNavBarShellState extends State<ScaffoldNavBarShell> {
                       onEnter: (event) => setState(() => _isHovered = true),
                       onExit: (event) => setState(() => _isHovered = false),
                       child: NavigationRail(
-                          destinations: _navRailDestinations,
+                          destinations: _buildNavRailDestinations(),
                           extended: context.isWidescreen || _isHovered,
                           onDestinationSelected: (index) => goToBranch(index),
                           selectedIndex: widget.navigationShell.currentIndex),
@@ -112,10 +67,58 @@ class ScaffoldNavBarShellState extends State<ScaffoldNavBarShell> {
           ? FinancrrNavigationBar(
               onDestinationSelected: (index) => goToBranch(index),
               selectedIndex: widget.navigationShell.currentIndex,
-              destinations: _navBarDestinations)
+              destinations: _buildNavBarDestinations())
           : null,
     );
   }
+
+  List<NavigationDestination> _buildNavBarDestinations() => [
+    NavigationDestination(
+      icon: const Icon(Icons.dashboard_outlined),
+      selectedIcon: const Icon(Icons.dashboard_rounded),
+      label: L10nKey.navigationDashboard.toString(),
+    ),
+    NavigationDestination(
+      icon: const Icon(Icons.account_balance_wallet_outlined),
+      selectedIcon: const Icon(Icons.account_balance_wallet_rounded),
+      label: L10nKey.navigationAccounts.toString(),
+    ),
+    NavigationDestination(
+      icon: const Icon(Icons.leaderboard_outlined),
+      selectedIcon: const Icon(Icons.leaderboard_rounded),
+      label: L10nKey.navigationStatistics.toString(),
+    ),
+    NavigationDestination(
+      icon: const Icon(Icons.person_outline_rounded),
+      selectedIcon: const Icon(Icons.person_rounded),
+      label: L10nKey.navigationSettings.toString(),
+    ),
+  ];
+
+  List<NavigationRailDestination> _buildNavRailDestinations() => [
+    NavigationRailDestination(
+      icon: const Icon(Icons.dashboard_outlined),
+      selectedIcon: const Icon(Icons.dashboard_rounded),
+      label: L10nKey.navigationDashboard.toText()
+    ),
+    NavigationRailDestination(
+      icon: const Icon(Icons.account_balance_wallet_outlined),
+      selectedIcon: const Icon(Icons.account_balance_wallet_rounded),
+      label: L10nKey.navigationAccounts.toText(),
+    ),
+    NavigationRailDestination(
+      icon: const Icon(Icons.leaderboard_outlined),
+      selectedIcon: const Icon(Icons.leaderboard_rounded),
+      label: L10nKey.navigationStatistics.toText(),
+    ),
+    NavigationRailDestination(
+      icon: const Icon(Icons.settings_outlined),
+      selectedIcon: const Icon(Icons.settings_rounded),
+      label: L10nKey.navigationSettings.toText(),
+    ),
+  ];
+
+  void refresh() => setState(() {});
 
   bool canPop() {
     final GoRouterState state = GoRouterState.of(context);
@@ -148,7 +151,6 @@ class FinancrrNavigationBar extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var theme = ref.watch(themeProvider);
-    final ThemeData themeData = theme.getActive().themeData;
 
     Widget buildNavBarItem({required NavigationDestination destination, required int index, bool isSelected = false}) {
       final IconData iconData = (destination.icon as Icon).icon!;
@@ -163,20 +165,20 @@ class FinancrrNavigationBar extends ConsumerWidget {
               padding: const EdgeInsets.all(8),
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                color: isSelected ? null : themeData.primaryColor.withOpacity(0.1),
+                color: isSelected ? null : theme.themeData.primaryColor.withOpacity(0.1),
                 border: Border.all(
-                  color: isSelected ? themeData.primaryColor : Colors.transparent,
+                  color: isSelected ? theme.themeData.primaryColor : Colors.transparent,
                   width: 4,
                 ),
               ),
-              child: Icon(isSelected ? selectedIconData : iconData, color: isSelected ? themeData.primaryColor : null),
+              child: Icon(isSelected ? selectedIconData : iconData, color: isSelected ? theme.themeData.primaryColor : null),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 5),
               child: Text(destination.label,
                   style: TextStyle(
                     fontSize: 12,
-                    color: isSelected ? themeData.primaryColor : null,
+                    color: isSelected ? theme.themeData.primaryColor : null,
                     fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
                   )),
             )
