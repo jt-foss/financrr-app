0ea2f9daafdce8453f556eebb3a7815e0883757b
jasonlessenich <jasonlessenich@gmail.com>
2024-05-14 11:07:58 +0000

    Deploying to gh-pages from @ financrr/financrr-app@f7e6db0b51f64efb28dd3cecd965ec5b6ac6727b 🚀

--- frontend-stage/flutter_bootstrap.js
+++ frontend-stage/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1670150968"
+    serviceWorkerVersion: "3305020719"
   }
 });
