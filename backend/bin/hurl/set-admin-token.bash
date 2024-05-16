850ca8ad579cdfc59dbbbed2d842c00b03986981
Denux <timon.klinkert@denux.dev>
2024-05-16 22:23:25 +0200

    hopefully fixed it this time (I swear I am losing my mind) (and round number 2)

--- backend/bin/hurl/set-admin-token.bash
+++ backend/bin/hurl/set-admin-token.bash
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 # prepare
-#set -e
+set -e
 set -x  # Add this line
 WORK_DIR="$(pwd)"
 cd "$(dirname "$0")"
 cd ../../
 
+set +e
 # Execute the curl command inside the hurl container and capture the token
 TOKEN=$(docker compose -f compose.yml -f compose.test.yml run -T --entrypoint curl hurl -s -X POST 'http://rust:8080/api/v1/session' \
     -H 'Content-Type: application/json' \
     -d '{"username": "admin", "password": "Financrr123", "session_name": "test_session"}' | jq -r '.token')
+set -e
 
 if [ "$TOKEN" != "null" ] && [ -n "$TOKEN" ]; then
     # Set the HURL_admin_token environment variable on the host machine
